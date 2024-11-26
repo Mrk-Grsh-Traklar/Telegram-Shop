aee7c27455982be7becfe6610213bcbd398caa76
Mrk-Grsh-Traklar <i29343229@gmail.com>
2024-11-26 19:44:39 +0300

    Delete ProjectShop directory

--- ProjectShop/imp.py
+++
@@ -1,4 +0,0 @@
-import telebot
-from telebot import types
-import sqlite3
-from template import *
--- ProjectShop/main.py
+++
@@ -1,5 +0,0 @@
-from bot_core import *
-
-botik = TelebotClass('7175732877:AAGKjHDw4DeTu5cfFeZIHCCRug2wxVAqcHM')
-print("started")
-botik.run()
